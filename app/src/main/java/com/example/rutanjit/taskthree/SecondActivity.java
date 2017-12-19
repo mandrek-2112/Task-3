504cc30565f5e80b5867ce33df5eb09194ea5cab
Rutanjit Mandrekar <rutman2112@gmail.com>
2017-12-20 00:08:21 +0530

    Task3 Created

--- app/src/main/java/com/example/rutanjit/taskthree/SecondActivity.java
+++ app/src/main/java/com/example/rutanjit/taskthree/SecondActivity.java
@@ -1,24 +1,24 @@
 package com.example.rutanjit.taskthree;
 
 import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
-    RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.cl);
 
         Intent intent = getIntent();
         Toast.makeText(this, "Task 3 Completed",
                 Toast.LENGTH_LONG).show();
         Snackbar snackbar;
-        snackbar = Snackbar.make(relativeLayout, "Second Activity", Snackbar.LENGTH_LONG);
+        snackbar = Snackbar.make(coordinatorLayout, "Second Activity", Snackbar.LENGTH_LONG);
         snackbar.show();
 
 
